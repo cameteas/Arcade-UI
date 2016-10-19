3bdaac271c63f0cf51a6ea877adcfc64cae0845b
cameteas <cameteas416@gmail.com>
2016-10-19 13:33:49 -0400

    second commit

--- TheUltimateArcade/Form1.cs
+++ TheUltimateArcade/Form1.cs
@@ -82,20 +82,113 @@ namespace WindowsFormsApplication1
             {
                 case Keys.Left:
                     gameValueHist = gameValue;
-                    gameValue++;
+                    if (gameValue == 1)
+                    {
+                        
+                    }
+                    else
+                    {
+                        gameValue--;
+                    }
                     break;
                 case Keys.Right:
                     gameValueHist = gameValue;
-                    gameValue--;
+                    if (gameValue == 3)
+                    {
+                        
+                    }
+                    else
+                    {
+                        gameValue++;
+                    }
+                    
                     break;
                 default:
                     rightPress = false;
                     leftPress = false;
                     break;
             }
+            title.Text = gameValue +"";
+            if (gameValue == 1)
+            {
+                snakeButton.Width = 192;
+                pongButton.Width = 0;
+                pacmanButton.Width = 0;
+                title.Text = "Snake";
+                description.Text = "Snake is the common name for a videogame concept where the player maneuvers a \nline which grows in length, with the line itself being a primary obstacle. The \nconcept originated in the 1976 arcade game Blockade, and the ease of \nimplementing Snake has led to hundreds of versions (some of which have the word \nsnake or worm in the title) for many platforms. There are over 300 for iOS alone.";
+            }
+            if (gameValue == 2)
+            {
+                snakeButton.Width = 0;
+                pongButton.Width = 192;
+                pacmanButton.Width = 0;
+                title.Text = "Pong";
+                description.Text = "The game was originally manufactured by Atari, which released it in 1972. Allan \nAlcorn created Pong as a training exercise assigned to him by Atari co-founder \nNolan Bushnell. ... As a result, Atari encouraged its staff to produce more innovative \ngames.";
+                if (gameValue == gameValueHist - 1)
+                {
+                    for (int a = 0; a < 100; a++)
+                    {
+                        if (a % 25 == 0)
+                        {
+                            pcBox1Y = pcBox1Y + 5;
+                            pcBox2Y = pcBox2Y - 5;
+                        }
+                        pcBox1X++;
+                        pcBox2X = pcBox2X + 2;
+                        pcBox3X = pcBox3X - 3;
 
-           
-            
+                        pictureBox1.Location = new Point(pcBox1X, pcBox1Y);
+                        pictureBox2.Location = new Point(pcBox2X, pcBox2Y);
+                        pictureBox3.Location = new Point(pcBox3X, pcBox3Y);
+                    }
+                }
+                if (gameValue == gameValueHist + 1)
+                {
+                    for (int a = 0; a < 100; a++)
+                    {
+                        if (a % 25 == 0)
+                        {
+                            pcBox1Y = pcBox1Y - 5;
+                            pcBox2Y = pcBox2Y + 5;
+                        }
+                        pcBox1X++;
+                        pcBox2X = pcBox2X + 2;
+                        pcBox3X = pcBox3X - 3;
+
+                        pictureBox1.Location = new Point(pcBox1X, pcBox1Y);
+                        pictureBox2.Location = new Point(pcBox2X, pcBox2Y);
+                        pictureBox3.Location = new Point(pcBox3X, pcBox3Y);
+                    }
+                }
+            }
+            if (gameValue == 3)
+            {
+                snakeButton.Width = 0;
+                pongButton.Width = 0;
+                pacmanButton.Width = 192;
+                title.Text = "Pacman";
+                description.Text = "Pac - Man(Japanese: パックマン Hepburn: Pakkuman ?) is an arcade game developed by \nNamco and first released in Japan in May 1980.It was created by Japanese video \ngame designer Toru Iwatani. It was licensed for distribution in the United States by \nMidway and released in October 1980.";
+                //if (gameValue == gameValueHist - 1)
+                //{
+                //    for (int a = 0; a < 100; a++)
+                //    {
+                //        if (a % 25 == 0)
+                //        {
+                //            pcBox1Y = pcBox1Y + 5;
+                //            pcBox2Y = pcBox2Y - 5;
+                //        }
+                //        pcBox1X++;
+                //        pcBox2X = pcBox2X + 2;
+                //        pcBox3X = pcBox3X - 3;
+
+                //        pictureBox1.Location = new Point(pcBox1X, pcBox1Y);
+                //        pictureBox2.Location = new Point(pcBox2X, pcBox2Y);
+                //        pictureBox3.Location = new Point(pcBox3X, pcBox3Y);
+                //    }
+                //}
+       
+            }
+            DoubleBuffered = true;
             Refresh();
         }
         private void snakeButton_Click(object sender, EventArgs e)
@@ -114,6 +207,16 @@ namespace WindowsFormsApplication1
         }
 
         private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void description_Click(object sender, EventArgs e)
         {
 
         }
